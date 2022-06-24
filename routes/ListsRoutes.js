2aa9b92f45d2045c4a5e327fcf0ee642254a42ad
Caitlin <cjcroteau@gmail.com>
2022-06-24 05:12:12 +0000

    update status codes, queries in ListsRoutes

--- routes/ListsRoutes.js
+++ routes/ListsRoutes.js
@@ -4,7 +4,6 @@ const router  = express.Router();
 ///add validation! see fruits model
 //https://github.com/pedroagont/webflex-lectures-21mar/blob/main/m05w12/controllers/FruitsController.js
 //add status codes
- //Add string and Values directly to db query
 
 //do we need to check the user_id of a specific list if each list has its own id?? probably not?
 //^ see queries delete, edit one list, view one list
@@ -15,17 +14,15 @@ module.exports = (db) => {
   router.post("/", (req, res) => {
     const { userId } = req.session;
     //add validation
+    const { name, icon_url } = req.body;
+    //add validation
 
-    const values = [userId, "name", "icon_url"]; //add name and icon_url
-    const queryString = `
-    INSERT into lists (user_id, name, icon_url)
-    VALUES ($1, $2, $3) RETURNING *
-    `;
-
-    db.query(queryString, values)
+    db.query(
+      `INSERT into lists (user_id, name, icon_url) VALUES ($1, $2, $3) RETURNING *`,
+      [userId, name, icon_url])
       .then(data => {
         const list = data.rows[0];
-        res.json({ message: "List created.", list });
+        res.status(201).json({ message: "List created.", list });
       })
       .catch(err => {
         res
@@ -41,16 +38,12 @@ module.exports = (db) => {
       return res.status(401).send("<h1>You are not logged in.</h1>");
     }
 
-    const queryString = `
-    SELECT * FROM lists
-    WHERE user_id = $1
-    `;
-    const values = [userId]
-
-    db.query(queryString, values)
+    db.query(
+      `SELECT * FROM lists WHERE user_id = $1`,
+      [userId])
       .then(data => {
         const lists = data.rows;
-        res.json({ message: "Here are all of your lists!", lists });
+        res.status(200).json({ message: "Here are all of your lists!", lists });
 
       })
       .catch(err => {
@@ -62,21 +55,16 @@ module.exports = (db) => {
 
   //view one list for user
   router.get("/:id", (req, res) => {
-    const { userId } = req.session;
     const { listId } = req.params.id;
-    //add validaition
+    const { userId } = req.session;
+    //add validation
 
-    const values = [listId, userId];
-    const queryString = `
-    SELECT * FROM lists
-    WHERE id = $1
-    AND user_id = $2
-    `;
-
-    db.query(queryString, values)
+    db.query(
+      `SELECT * FROM lists WHERE id = $1 AND user_id = $2`,
+      [listId, userId])
       .then(data => {
         const list = data.rows[0];
-        res.json({ message: "Here is your list.", list });
+        res.status(200).json({ message: "Here is your list.", list });
       })
       .catch(err => {
         res
@@ -87,24 +75,16 @@ module.exports = (db) => {
 
   //edit one list
   router.put("/:id", (req, res) => {
-    const { userId } = req.session;
     const { listId } = req.params.id;
+    const { name, icon_url } = req.body //is this correct?
     //add validaition
 
-    //where am i pulling new name or new icon url from? form on front end
-    const values = [listId, userId, "new name", "new icon url"];
-    const queryString = `
-    UPDATE lists
-    SET THE VALUES LATER
-    WHERE id = $1
-    AND user_id = $2
-    `;
-    //update this query string ^
-
-    db.query(queryString, values)
+    db.query(
+      `UPDATE lists SET THE VALUES LATER WHERE id = $1`, //set the values here
+      [listId, name, icon_url])
       .then(data => {
         const list = data.rows[0];
-        res.json({ message: "List updated.", list });
+        res.status(200).json({ message: "List updated.", list });
       })
       .catch(err => {
         res
@@ -115,17 +95,12 @@ module.exports = (db) => {
 
   //deletes one list
   router.delete("/:id", (req, res) => {
-    const { userId } = req.session;
     const { listId } = req.params.id;
+
     //add validaition
 
-    const values = [listId, userId];
-    const queryString = `
-    DELETE FROM lists
-    WHERE id = $1
-    `;
-
-    db.query(queryString, values)
+    db.query(`DELETE FROM lists WHERE id = $1`,
+    [listId])
       .then(data => {
         // const list = data.rows[0];
         res.status(204).json({ message: "List deleted." })
