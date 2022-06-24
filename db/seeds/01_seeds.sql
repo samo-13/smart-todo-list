af425409fd80c2a8770de1addeb715e26209f27c
Billy Wong <bwong.yh@protonmail.com>
2022-06-25 00:14:20 +0900

    create dummy users
    delete default widgets and users data

---
+++ db/seeds/01_seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (name, email, password, avatar_url) VALUES 
+  ('Caitlin Croteau', 'caitlin-croteau@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', null),
+  ('Sarah Moss', 'sarah.moss@outlook.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', null),
+  ('Billy Wong', 'billywong@outlook.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', null);
