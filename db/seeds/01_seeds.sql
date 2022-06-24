36ba1a8176cbb9e60fa66d16b02bb8d05c531f40
Billy Wong <bwong.yh@protonmail.com>
2022-06-25 00:26:10 +0900

    insert categories data

--- db/seeds/01_seeds.sql
+++ db/seeds/01_seeds.sql
@@ -2,3 +2,9 @@ INSERT INTO users (name, email, password, avatar_url) VALUES
   ('Caitlin Croteau', 'caitlin-croteau@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', null),
   ('Sarah Moss', 'sarah.moss@outlook.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', null),
   ('Billy Wong', 'billywong@outlook.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', null);
+
+INSERT INTO categories (name) VALUES 
+  ('Film / Series'),
+  ('Restaurants, cafes, etc.'),
+  ('Books'),
+  ('Products');
