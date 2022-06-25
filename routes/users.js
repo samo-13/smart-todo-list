e627fa8cfbedf49017697b34f31f9c72eb08aee4
Billy Wong <bwong.yh@protonmail.com>
2022-06-25 23:58:13 +0900

    refactor POST /login

--- routes/users.js
+++ routes/users.js
@@ -11,11 +11,14 @@ const router = express.Router();
 
 module.exports = (db) => {
   router.post('/login', (req, res) => {
-    const {email, password} = req.body;
+    // const {email, password} = req.body;
+    const email = 'billywong@outlook.com';
+    const password = 'password';
 
     db.query(`SELECT * FROM users WHERE email = $1`, [email])
       .then(data => {
         if (data.rowCount > 0 && bcrypt.compareSync(password, data.rows[0].password)) {
+          req.session.userId = data.rows[0].id; // set cookies
           res.send(data.rows[0]);
         } else {
           res.status(400).send('<h1>Please check your email and password</h1>');
