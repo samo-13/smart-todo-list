272068b7974ee553d884e0a4ee5d876233650d5c
Billy Wong <bwong.yh@protonmail.com>
2022-06-26 00:45:05 +0900

    complete POST /register

--- routes/users.js
+++ routes/users.js
@@ -7,9 +7,11 @@
 
 const bcrypt = require('bcrypt');
 const express = require('express');
+const {reset} = require('nodemon');
 const router = express.Router();
 
 module.exports = (db) => {
+  // login routes
   router.post('/login', (req, res) => {
     // const {email, password} = req.body;
     const email = 'billywong@outlook.com';
@@ -47,6 +49,31 @@ module.exports = (db) => {
     } else {
       res.status(400).send('<h1>Please log in.</h1>');
     };
+  });
+
+  // register routes
+  router.post('/register', (req, res) => {
+    // const {name, email, password, avatar_url} = req.body
+    const [name, email, password, avatar_url] = ['Jon Smith', 'jon-smith3@hotmail.com', 'myPassword', null];
+
+    if (!name || !email || !password) return res.status(400).send('<h1>Please fill in all necessary fields.</h1>');
+
+    db.query(`SELECT * FROM users WHERE email = $1`, [email])
+      .then(data => {
+        if (data.rowCount > 0) {
+          res.status(400).send('<h1>This email has been registered.</h1>');
+        } else {
+          db.query(`INSERT INTO users (name, email, password, avatar_url) VALUES ($1, $2, $3, $4) RETURNING *`, [name, email, bcrypt.hashSync(password, 10), avatar_url])
+            .then(data => {
+              res.status(201).send(data.rows[0]);
+            })
+            .catch(err => {
+              res
+                .status(500)
+                .json({error: err.message});
+            });
+        }
+      });
   });
 
   return router;
