9576d7272a264bd517c57545e81c939715ff9e8d
Sarah Moss <sarah-moss@live.com>
2022-06-26 14:37:10 +0000

    Complete post route for tasks

--- routes/TasksRoutes.js
+++ routes/TasksRoutes.js
@@ -6,15 +6,17 @@ module.exports = (db) => {
   // --------------------------------------------------------------------------------------------------
   // GET /task --- view form to create task
 
-  router.get("/", (req, res) => { // /task isn't needed - use just /
+  // router.get("/", (req, res) => { // /task isn't needed - use just /
+  //   // return all tasks that belong to a users list
 
-  });
+  // });
 
   // --------------------------------------------------------------------------------------------------
   // POST /task --- create new task
 
   router.post("/", (req, res) => { // /task isn't needed - use just /
     // uncomment line below when app is ready + remove dummy data
+    // console.log('REQ.SESSION:', req.session);
     // const { userId } = req.session;
 
     // dummy data without priority
@@ -23,6 +25,7 @@ module.exports = (db) => {
     const name = 'Seinfeld'
     const create_at = '2022-05-02'
     const priority = false
+    const userId = 1;
 
     // dummy data with priority
     // const list_id = 3
@@ -38,15 +41,18 @@ module.exports = (db) => {
 
     // uncomment line below when app is ready + remove dummy data
     // const { list_id, category_id, name, create_at, priority } = req.body; // do we include category_id here? we will be using API to generate
+    // category_id will likely not be needed depending if we use a method to get category or not
     if (!list_id || !category_id  || !name || !create_at) { // include priority? will either be true or false as it's optional
       return res.status(401).send("<h1>Please ensure all required fields are populated!</h1>"); // can change to be more specific
     }
 
+    // run database query, then
     db.query(
       `INSERT into tasks (list_id, category_id, name, create_at, priority) VALUES ($1, $2, $3, $4, $5) RETURNING *`,
-      [list_id, category_id, name, create_at, priority])
+      [list_id, category_id, name, create_at, priority]
+      )
       .then(data => {
-        const task = data.rows[0];
+        const task = data.rows[0]; // array comes back as recently created task
         res.status(201).json({ message: "Task created.", task });
       })
       .catch(err => {
@@ -55,6 +61,8 @@ module.exports = (db) => {
           .json({ error: err.message });
       });
   });
+
+
 
   // --------------------------------------------------------------------------------------------------
   // PUT /task/:id --- edit one task
