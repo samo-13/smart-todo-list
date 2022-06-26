38020bbb83bee24b82373a0802b7d05c2353c9b6
Sarah Moss <sarah-moss@live.com>
2022-06-26 15:16:53 +0000

    Add tasks put route

--- routes/TasksRoutes.js
+++ routes/TasksRoutes.js
@@ -68,7 +68,40 @@ module.exports = (db) => {
   // PUT /task/:id --- edit one task
 
   router.put("/:id", (req, res) => { // /task/:id isn't needed - use just /:id
+    // const { taskId } = req.params.id;
+    // const { list_id, category_id, name, create_at, priority } = req.body; //is this correct?
+    // const { userId } = req.session;
 
+    // dummy data
+    const taskId = 1
+    const list_id = 1
+    const category_id = 1
+    const name = 'Changed Name'
+    const create_at = '2022-05-02'
+    const priority = false
+    const userId = 1;
+
+    if (!userId) {
+      return res.status(401).send("<h1>You are not logged in.</h1>");
+    }
+
+    db.query(
+      // `UPDATE tasks SET name = $2, category_id = $3, name = $4, create_at = $5, priority = $6 WHERE id = $1`,
+      `UPDATE tasks SET category_id = $2, name = $3, create_at = $4, priority = $5 WHERE id = $1`, // do wew want to update create_at on edit?
+      [taskId, category_id, name, create_at, priority]
+      )
+      .then(data => {
+        const task = data.rows[0];
+        if (!task) {
+          return res.status(404).send("<h1>Task not found!</h1>");
+        }
+        res.status(200).json({ message: "Task updated.", list });
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .json({ error: err.message });
+      });
   });
 
   // --------------------------------------------------------------------------------------------------
