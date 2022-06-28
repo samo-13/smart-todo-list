de7c5f0f081b30073c799944e8f05bc6dd9aced4
Billy Wong <bwong.yh@protonmail.com>
2022-06-29 02:17:21 +0900

    remove comments

--- routes/UsersRoutes.js
+++ routes/UsersRoutes.js
@@ -1,10 +1,3 @@
-/*
- * All routes for Users are defined here
- * Since this file is loaded in server.js into api/users,
- *   these routes are mounted onto /users
- * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
- */
-
 const bcrypt = require('bcrypt');
 const express = require('express');
 const router = express.Router();
