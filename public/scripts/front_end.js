c5e1ba067aee36e04ae2af2917103316511f0b69
Sarah Moss <sarah-moss@live.com>
2022-06-29 18:07:09 +0000

    Add close modal functionality

--- public/scripts/front_end.js
+++ public/scripts/front_end.js
@@ -3,14 +3,21 @@ const modal = document.getElementById("createListModal")
 const modalLink = document.getElementById("modalLink")
 
 // Returns a reference to the first object with the specified value of the ID attribute.
-const closeModalButton = document.getElementsByClassName("close-modal-button")
+const closeModalButton = document.getElementById("closeModalButton")
 
-// open the modal by blocking the display: none css value
+// open the modal changing the display: none to display: block css value
 let displayModal = function () {
   console.log('Hello from displayModal function')
   modal.style.display = 'block';
 }
 
+// close the modal by enabling the display: none css value
+let exitModal = function () {
+  console.log('Hello from closeModal function')
+  modal.style.display = 'none';
+}
+
 // listen for modalLink click
-modalLink.addEventListener('click', displayModal)
+modalLink.addEventListener('click', displayModal);
+closeModalButton.addEventListener('click', exitModal);
 
