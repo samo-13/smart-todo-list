fcef6b2de51e3626691ff04dd38290bb06c14a4e
Billy Wong <bwong.yh@protonmail.com>
2022-06-30 02:03:35 +0900

    link api res to new task

--- routes/TasksRoutes.js
+++ routes/TasksRoutes.js
@@ -15,12 +15,6 @@ module.exports = (db) => {
   // --------------------------------------------------------------------------------------------------
   const generateCategory = require('../lib/generateCategory');
 
-  router.post("/test", (req, res) => {
-    generateCategory('watch harry potter')
-      .then(re => res.send(re.data.choices[0].text))
-      .catch(err => console.log(err));
-  });
-
   // POST /task --- create new task
   router.post("/", (req, res) => { // /task isn't needed - use just /
     // uncomment line below when app is ready + remove dummy data
@@ -28,19 +22,19 @@ module.exports = (db) => {
     // const { userId } = req.session;
 
     // dummy data without priority
-    const list_id = 1;
-    const category_id = 1;
-    const name = 'Stranger Things';
-    const create_at = '2022-05-02';
-    const priority = false;
+    // const list_id = 1;
+    // // const category_id = 1;
+    // const name = 'Stranger Things';
+    // const create_at = '2022-05-02';
+    // const priority = false;
     const userId = 1;
 
-    // dummy data with priority
-    // const list_id = 3
-    // const category_id = 2
-    // const name = 'Taco Bell'
-    // const create_at = '2022-05-02'
-    // const priority = true
+    // // dummy data with priority
+    const list_id = 3;
+    // const category_id = 2;
+    const name = 'Taco Bell';
+    const create_at = '2022-05-02';
+    const priority = true;
 
     // make sure user is logged in
     if (!userId) {
@@ -50,25 +44,58 @@ module.exports = (db) => {
     // uncomment line below when app is ready + remove dummy data
     // const { list_id, category_id, name, create_at, priority } = req.body; // do we include category_id here? we will be using API to generate
     // category_id will likely not be needed depending if we use a method to get category or not
-    if (!list_id || !category_id || !name || !create_at) { // include priority? will either be true or false as it's optional
+    if (!list_id || /*!category_id ||*/ !name || !create_at) { // include priority? will either be true or false as it's optional
       return res.status(401).send("<h1>Please ensure all required fields are populated!</h1>"); // can change to be more specific
     }
 
-    // run database query, then
-    db.query(
-      `INSERT into tasks (list_id, category_id, name, create_at, priority) VALUES ($1, $2, $3, $4, $5) RETURNING *`,
-      [list_id, category_id, name, create_at, priority]
-    )
-      .then(data => {
-        console.log('CONSOLE 1:', data.rows[0]);
-        const task = data.rows[0]; // array comes back as recently created task
-        res.status(201).json({message: "Task created.", task});
+    const categories = ['Film / Series', 'Books', 'Restaurants / Cafes / etc.', 'Products'];
+    // fetch openai to sort new task into appropriate category
+    generateCategory(name)
+      .then(resp => {
+        categories.forEach(category => {
+          if (resp.data.choices[0].text.includes(category)) {
+            // query db to retrieve category_id 
+            db.query(`SELECT id FROM categories WHERE name = $1`, [category])
+              .then(data => {
+                const category_id = data.rows[0].id;
+
+                // make new task and insert it to db as soon as category_id is found
+                db.query(
+                  `INSERT into tasks (list_id, category_id, name, create_at, priority) VALUES ($1, $2, $3, $4, $5) RETURNING *`,
+                  [list_id, category_id, name, create_at, priority]
+                )
+                  .then(data => {
+                    console.log('CONSOLE 1:', data.rows[0]);
+                    const task = data.rows[0]; // array comes back as recently created task
+                    res.status(201).json({message: "Task created.", task});
+                  })
+                  .catch(err => {
+                    res
+                      .status(500)
+                      .json({error: err.message});
+                  });
+              });
+          }
+        });
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({error: err.message});
-      });
+      .catch(err => console.log(err));
+
+    // const category_id = null;
+    // run database query, then
+    //   db.query(
+    //     `INSERT into tasks (list_id, category_id, name, create_at, priority) VALUES ($1, $2, $3, $4, $5) RETURNING *`,
+    //     [list_id, category_id, name, create_at, priority]
+    //   )
+    //     .then(data => {
+    //       console.log('CONSOLE 1:', data.rows[0]);
+    //       const task = data.rows[0]; // array comes back as recently created task
+    //       res.status(201).json({message: "Task created.", task});
+    //     })
+    //     .catch(err => {
+    //       res
+    //         .status(500)
+    //         .json({error: err.message});
+    //     });
   });
 
 
