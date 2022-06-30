cf0d861ea2bf44d119e8d9c5d39f8b01c1190e52
Sarah Moss <sarah-moss@live.com>
2022-06-30 22:10:25 +0000

    Push recent changes from mentor support

--- public/scripts/list_show.js
+++ public/scripts/list_show.js
@@ -3,18 +3,27 @@
 // ------------------------------------------------------------------------------------------------
 // show one list in individual container
 // ------------------------------------------------------------------------------------------------
+let taskName
+let listName
+let taskId
+let listId
+
 
 $(document).ready(function() {
 
-  console.log('HI, FROM DOCUMENT.READY FUNCTION')
+  console.log('HI, FROM DOCUMENT.READY FUNCTION');
+
+  // const getTaskName = function(element, taskName) {
+  //   element.val(taskName)
+  // };
 
   const createListElement = (task) => {
-    console.log('Inside createListElement')
+    console.log('Inside createListElement');
 
-    const listName = task.list_name;
-    const taskName = task.task_name;
-    const taskId = task.task_id
-    const listId = $("#list_id").val();
+    listName = task.list_name;
+    taskName = task.task_name;
+    taskId = task.task_id;
+    listId = $("#list_id").val();
     console.log('listName', listName);
     console.log('taskName', taskName);
     console.log('taskId', taskId);
@@ -36,44 +45,46 @@ $(document).ready(function() {
           </div>
           </article>
         </div>
-
-
-        <script>
-            $('.list').on("click", '#editTaskIconLink-${taskId}', function() {
-
-              console.log('The edit icon was clicked!');
-              console.log('TASK ID:', ${taskId})
-
-
-              const closeEditModalButton = document.getElementById("closeEditModalButton");
-
-              let displayEditModal = function() {
-                console.log('Hello from displayEditModal function');
-                editTaskModal.style.display = 'block';
-              };
-
-              let exitEditModal = function() {
-                console.log('Hello from closeEditModal function');
-                editTaskModal.style.display = 'none';
-              };
-
-              displayEditModal();
-              closeEditModalButton.addEventListener('click', exitEditModal);
-            });
-
-
-        </script>
-      `);
-
-    console.log('createListElement:', $list)
+        `);
+    console.log('createListElement:', $list);
 
     return $list;
+
   };
+
+
+  $('.list').on("click", `#editTaskIconLink-${taskId}`, function() {
+
+    console.log('TASK:', task);
+
+    console.log($("#editTaskNameId"));
+
+    $('#editTaskNameId').text(taskName);
+
+    console.log('The edit icon was clicked!');
+    console.log('TASK NAME:', taskName);
+
+    const closeEditModalButton = document.getElementById("closeEditModalButton");
+
+    let displayEditModal = function() {
+      console.log('Hello from displayEditModal function');
+      editTaskModal.style.display = 'block';
+    };
+
+    let exitEditModal = function() {
+      console.log('Hello from closeEditModal function');
+      editTaskModal.style.display = 'none';
+    };
+
+    displayEditModal();
+    closeEditModalButton.addEventListener('click', exitEditModal);
+  });
+
 
   // IMPLEMENT TO LOAD TASKS/ONE LIST USING AJAX (SIMILAR TO TWEETER)
 
   const loadList = function() {
-    console.log('loadList function')
+    console.log('loadList function');
     const listId = $("#list_id").val();
 
     $.ajax({
@@ -82,7 +93,7 @@ $(document).ready(function() {
       dataType: "json"
     })
       .then(function(list) {
-        $("#list_name").text(list.list[0].list_name)
+        $("#list_name").text(list.list[0].list_name);
         //grabbing list name element from the DOM and setting its text
 
         console.log('Success: See list', list.list[0]);
@@ -93,7 +104,7 @@ $(document).ready(function() {
   const renderList = (listData) => {
     //loops through tasks and appends
 
-    const list = listData.list
+    const list = listData.list;
     console.log("list data", list);
 
     for (let i = 0; i < list.length; i++) { // loops through list
@@ -105,10 +116,11 @@ $(document).ready(function() {
 
       console.log('created element task', task);
       $('.list').append(task);// appending to the DOM
-      //takes return value and appends it to the listscontainer
+      // takes return value and appends it to the listscontainer
       // to add it to the page so we can make sure it's got all the right elements, classes, etc.
     }
-
   };
-    loadList()
-  });
+
+  loadList();
+
+});
